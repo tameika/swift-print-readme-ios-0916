fc8e11d91a8785520dcb7cff2fc60e65108c6aa1
Tameika Lawrence <tameikalawrence@Tameikas-MacBook-Air.local>
2016-08-30 08:56:44 -0400

    print

---
+++ Print.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favoriteWord = "food"
+var favoriteColor = "blue"
+var favoriteMusician = "drake"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print ("The sky is \(color).")
+
+print("Hi mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)"
+print(skySentence)
+
